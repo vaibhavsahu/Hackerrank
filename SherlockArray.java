dcf4ff7f7edbd157723be5d8887125b9d5d6845a
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-09-05 11:34:58 -0600

    Hackerrank Sherock Array
    Java

---
+++ SherlockArray.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the balancedSums function below.
+    static String balancedSums(List<Integer> arr) {
+        int sum = 0;
+        
+        for(int i = 0; i < arr.size(); i++){
+            sum += arr.get(i);
+        }
+        
+        int curr = 0;
+        
+        for(int j = 0; j < arr.size(); j++){
+            if(curr == sum - arr.get(j) - curr){
+                return "YES";
+            }
+            curr += arr.get(j);
+        }
+        
+        return "NO";
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int T = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, T).forEach(TItr -> {
+            try {
+                int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+                List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                    .map(Integer::parseInt)
+                    .collect(toList());
+
+                String result = balancedSums(arr);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
