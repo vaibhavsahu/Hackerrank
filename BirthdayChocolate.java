dda5095954d931d1e0408f9bc2738a942256b58b
Vaibhav Sahu <vaibh3jec@gmail.com>
2018-09-05 11:39:26 -0600

    Hackerrank Birthday Chocolate
    Java

---
+++ BirthdayChocolate.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+        int ways = 0;
+        int sum = 0;
+        
+        if(m <= s.size()){
+            for(int i = 0; i < m; i++){
+                sum += s.get(i);
+            }
+        }
+        
+        if(sum == d)
+            ways++;
+        
+        //use sliding window
+        
+        for(int j = 0; j <s.size() - m; j++){
+            sum = sum - s.get(j) + s.get(j+m);
+            if(sum == d) ways++;
+        }
+    return ways;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
